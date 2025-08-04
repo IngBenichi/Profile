008e0360b3891fede57d606447afb714aa17edc2
Camilo Benitez <cbenitez8@cuc.edu.co>
2025-08-04 10:47:20 -0500

    Fix: File app/contact/route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: Request) {
       },
     });
 
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+    const baseUrl = process.env.NEXT_PRIVATE_BASE_URL || 'http://localhost:3000';
     const logoUrl = `${baseUrl}/logo.jpg`;
     const mailOptions = {
       from: process.env.SMTP_USER,
